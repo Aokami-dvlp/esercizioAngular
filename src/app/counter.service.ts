14d69340606b4c624072414b08578903c8ea0e9c
Aokami-dvlp <emanuele.david3@gmail.com>
2021-12-01 13:57:34 +0100

    Update counter.service.ts

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class CounterService {
 
-counter:number = 0;
+private counter:number = 0;
 value:number = 0;
 
 show(){
