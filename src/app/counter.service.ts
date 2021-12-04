08ffc83ca1591634b4698b6e509a43aa8aa2ddaa
Aokami-dvlp <emanuele.david3@gmail.com>
2021-12-04 18:16:08 +0100

    Update counter.service.ts

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -11,12 +11,12 @@ show(): Observable<number> {
   return this.counterSubject;
 }
 
-add(value:number = 1):void{
+add(value:number):void{
   this.counterSubject.next(this.counter += value);
 }
 
-sub(value:number = 1):void{
-  if((this.counter -= value) < 0){
+sub(value:number):void{
+  if(this.counter < value){
     alert("Non puoi andare sotto zero")
   } else {
     this.counterSubject.next(this.counter -= value)
