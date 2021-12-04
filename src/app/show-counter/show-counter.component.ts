3a1d0c95f247bf9f05d97b45dbbaa6a6f5d2f839
Aokami-dvlp <emanuele.david3@gmail.com>
2021-12-04 14:26:45 +0100

    Update show-counter.component.ts

--- src/app/show-counter/show-counter.component.ts
+++ src/app/show-counter/show-counter.component.ts
@@ -8,8 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowCounterComponent implements OnInit {
 
-/* counter:number|string = this.counterService.show();
- */
   constructor(public counterService:CounterService) {
   }
 
